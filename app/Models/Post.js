c868e13ee940776aab6e4c842d1b3721bf3a1c99
Thiago Pereira C <thiagoCord755@gmail.com>
2018-09-16 17:56:18 -0400

    Added reply properties to Post model

--- app/Models/Post.js
+++ app/Models/Post.js
@@ -4,7 +4,9 @@ const postSchema = new mongoose.Schema({
 	title: String,
 	body: String,
 	likes: { type: Number, default: 0 },
-	createdOn: { type: Date, default: Date.now }
+	createdOn: { type: Date, default: Date.now },
+	parent: { type: mongoose.Schema.Types.ObjectId, Ref: 'Post' },
+	replies: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Post', postSchema);
